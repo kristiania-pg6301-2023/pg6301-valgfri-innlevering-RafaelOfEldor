44c8fa8023d99a15af1e5c9bd437388f282e0cff
Letrix7 <letrix7@gmail.com>
2023-10-17 05:36:55 +0200

    removed some outdated comments and code

--- todo-innlevering/server/server.js
+++ todo-innlevering/server/server.js
@@ -9,13 +9,6 @@ app.listen(process.env.PORT || 3000);
 
 export const goalsApi = express.Router();
 
-// let TASKS = [
-//   {
-//     id: 1,
-//     goal: "Example goal #1",
-//   }
-// ];
-
 
 const url = "mongodb+srv://letrix7:D3tV4r3Ng4Ng@cluster0.ps27mqb.mongodb.net/?retryWrites=true&w=majority";
 
@@ -57,23 +50,10 @@ client.connect().then((connection) => {
     .collection("goals")
     .insertOne(req.body)
   res.end();
-  });
-
-  // goalsApi.get("/api/tasks", async (req, res) => {
-  //   console.log("im here")
-  //   const goals = await connection
-  //     .db("todo-list")
-  //     .collection("test-goals")
-  //     .find()
-  //     .toArray();
-  //   res.json(goals)
-  // });
-  
-  
+  });  
 })
 
 goalsApi.get("/api/tasks", async (req, res) => {
-  console.log("im here")
   const goals = await client
     .db("todo-list")
     .collection("test-goals")
@@ -111,66 +91,3 @@ goalsApi.post("/api/tasks", async (req, res) => {
 res.sendStatus(204)
 res.end();
 });
-
-// app.put("/api/tasks/:id", async (req, res) => {
-//   console.log("hii")
-//   const id = parseInt(req.params.id);
-//   const { goal } = req.body;
-//   await connection
-//   .db("todo-list")
-//   .collection("goals")
-//   .findOneAndUpdate({id: id}, {goal: goal}, (error, data) => {
-//     if(error){
-//       console.log(error)
-//     } else {
-//       console.log(data)
-//     }
-//   })
-// // console.log(goals)
-// // res.json(goals)
-// res.end();
-// });
-
-// app.put("/api/tasks/:id", (req, res) => {
-//   const id = parseInt(req.params.id);
-//   const { goal } = req.body;
-//   TASKS.find((item) => item.id === id).goal = goal;
-
-//   res.end();
-// });
-
-// app.delete("/api/tasks/:id", (req, res) => {
-//   const elementId = parseInt(req.params.id);
-//   TASKS = TASKS.filter(({ id }) => id !== elementId);
-//   res.end();
-// });
-
-// app.post("/api/tasks", (req, res) => {
-//   console.log("im here");
-//   TASKS.push(req.body);
-//   res.end();
-// });
-
-// goalsApi.get("/api/tasks", (req, res) => {
-//   console.log("im here")
-//   res.json(TASKS);
-// });
-
-// goalsApi.put("/api/tasks/:id", (req, res) => {
-//   const id = parseInt(req.params.id);
-//   const { goal } = req.body;
-//   TASKS.find((item) => item.id === id).goal = goal;
-//   res.sendStatus(204);
-//   res.end();
-// });
-
-// goalsApi.delete("/api/tasks/:id", (req, res) => {
-//   const elementId = parseInt(req.params.id);
-//   TASKS = TASKS.filter(({ id }) => id !== elementId);
-// });
-
-// goalsApi.post("/api/tasks", (req, res) => {
-//   TASKS.push(req.body);
-//   res.sendStatus(204);
-//   res.end();
-// });
